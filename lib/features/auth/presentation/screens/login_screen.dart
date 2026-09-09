e89691368876d665f9458ec06342340186be1df7
ataha4swapp <ataha@4sw.app>
2026-09-09 22:41:21 +0300

    remove unused import

--- lib/features/auth/presentation/screens/login_screen.dart
+++ lib/features/auth/presentation/screens/login_screen.dart
@@ -18,7 +18,6 @@ import '../../../../shared/providers/shared_providers.dart';
 import '../../../../shared/utils/location_permission_prompt.dart';
 import '../../../../shared/widgets/app_snackbar.dart';
 import '../providers/auth_provider.dart';
-import '../providers/guest_mode_provider.dart';
 import '../providers/phone_auth_provider.dart';
 import '../../../../shared/widgets/xstore_button.dart';
 import '../widgets/auth_divider.dart';
